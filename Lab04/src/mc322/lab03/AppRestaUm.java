5e73579c2a7e20f7468b1d9c0f03f279a0e95ce5
Jéssica Oliveira <jessica.oliveira@conpec.com.br>
2021-04-19 20:27:20 -0300

    feito o básico para checkpoint

--- Lab04/src/mc322/lab03/AppRestaUm.java
+++ Lab04/src/mc322/lab03/AppRestaUm.java
@@ -1,5 +1,8 @@
 package mc322.lab03;
 
 public class AppRestaUm {
-
+	public static void main(String args[]) {
+		Tabuleiro resta1 = new Tabuleiro();
+		resta1.Apresentar();
+	}
 }
--- Lab04/src/mc322/lab03/Pino.java
+++ Lab04/src/mc322/lab03/Pino.java
@@ -1,5 +1,23 @@
 package mc322.lab03;
 
 public class Pino {
-
+	int linha, coluna;
+	boolean emJogo;
+	
+	Pino (int linha, int coluna){
+		this.linha = linha;
+		this.coluna = coluna;
+		emJogo = true;
+	}
+	
+	void Mover(int linha, int coluna) {
+		this.linha = linha;
+		this.coluna = coluna;
+	}
+	
+	void Sair() {
+		emJogo = false;
+	}
+	
+	
 }

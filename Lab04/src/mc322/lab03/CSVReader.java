4e09374012006bdd8224b4b50d9edbe30f8053cc
Jéssica Oliveira <jessica.oliveira@conpec.com.br>
2021-04-20 18:44:09 -0300

    inicio csv

---
+++ Lab04/src/mc322/lab03/CSVReader.java
@@ -0,0 +1,47 @@
+package mc322.lab03;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CSVReader {
+	private String dataSource;
+    private String[] commands;
+
+      public CSVReader() {
+            this.commands = null;
+            this.dataSource = null;
+      }
+
+      public String getDataSource() {
+            return dataSource;
+      }
+
+      public void setDataSource(String dataSource) {
+            this.dataSource = dataSource;
+            if (dataSource == null) {
+              commands = null;
+        } else
+              readCSV();
+      }
+
+
+      public String[] requestCommands() {
+            return commands;
+      }
+
+      private void readCSV() {
+            try {
+                  BufferedReader file = new BufferedReader(new FileReader(dataSource));
+    
+                  String line = file.readLine();
+                  if (line != null) {
+                    commands = line.split(",");
+                    line = file.readLine();
+              }
+              file.close();
+            } catch (IOException erro) {
+              erro.printStackTrace();
+            }
+     }
+
+}

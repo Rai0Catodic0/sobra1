8c0bacd34c50d59e35fa7782df8ee2e5a02248e2
Jéssica Oliveira <jessica.oliveira@conpec.com.br>
2021-04-20 20:31:49 -0300

    mudei a estrutura dos pinos e tabuleiros, coloquei criei a função mover com ints como parâmetro, csv segue com problemas

--- Lab04/src/mc322/lab03/AppRestaUm.java
+++ Lab04/src/mc322/lab03/AppRestaUm.java
@@ -4,9 +4,9 @@ public class AppRestaUm {
 	public static void main(String args[]) {
 		Tabuleiro resta1 = new Tabuleiro();
 		resta1.Apresentar();
-		CSVReader csv = new CSVReader();
-	    csv.setDataSource("/home/jessica/sobra1/Lab03/src/db");
-	    String commands[] = csv.requestCommands();
-	    System.out.print(commands[0]);
+		resta1.Mover(1, 3, 4, 3);
+		resta1.Apresentar();
+		resta1.Mover(1, 3, 3, 3);
+		resta1.Apresentar();
 	}
 }
--- Lab04/src/mc322/lab03/Tabuleiro.java
+++ Lab04/src/mc322/lab03/Tabuleiro.java
@@ -1,4 +1,5 @@
 package mc322.lab03;
+import java.lang.Math;
 
 public class Tabuleiro {
 	Pino tabuleiro[][] = new Pino[7][7];
@@ -9,28 +10,64 @@ public class Tabuleiro {
 				if((i<2 && j>4)||(i<2 && j<2) || (i>4 && j<2) || (i>4 && j>4)) {
 					tabuleiro[i][j] = null;
 				} else if(i==3 && j==3) {
-					tabuleiro[i][j] = new Pino(i,j);
-					tabuleiro[i][j].emJogo = false;
+					tabuleiro[i][j] = null;
 				}
 				else {
 					tabuleiro[i][j] = new Pino(i, j);
 				}
 			}
 		}
+		
+	}
+	
+	boolean EstaNoTabuleiro(int i, int j){
+		if((j>1 && j<5)||(i>1 && i<5)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 	
 	void Apresentar() {
 		for(int i=0;i<7;i++) {
 			for(int j = 0; j<7; j++) {
-				if(tabuleiro[i][j]==null) {
+				if(!EstaNoTabuleiro(i,j)) {
 					System.out.print(" ");
-				} else if(tabuleiro[i][j].emJogo) {
-					System.out.print("P");
-				} else {
+				} else if(tabuleiro[i][j]==null) {
 					System.out.print("-");
+				} else {
+					System.out.print("P");
 				}
 			}
 			System.out.print("\n");
 		}
+	}
+	
+	void Mover(int iOrigem, int jOrigem, int iDestino, int jDestino) {
+		//validar movimento
+		if(!EstaNoTabuleiro(iDestino,jDestino)||!EstaNoTabuleiro(iDestino, jDestino)) {
+			System.out.println("Casas inválidas!");
+		} else if(tabuleiro[iDestino][jDestino]!=null) {
+			System.out.println("Casa de destino inválida");
+		} else if((iOrigem!=iDestino)&& (jDestino!=jOrigem)){
+			System.out.println("Movimento na Diagonal Inválido");
+		} else if(Math.abs(iOrigem-iDestino)>1 && Math.abs(jOrigem-jDestino)>1){
+			System.out.println("Você está pulando muitas casas!");
+		} else {
+			System.out.println("Movimento válido:");
+			tabuleiro[iDestino][jDestino] = tabuleiro[iOrigem][jOrigem];
+			tabuleiro[iOrigem][jOrigem]= null;
+			//movimento com peças na mesma linha
+			if(iDestino==iOrigem) {
+				int jFora = (jDestino+jOrigem)/2;
+				int iFora = iOrigem;
+				tabuleiro[iFora][jFora] = null;
+			} else { //movimento na mesma coluna
+				int iFora = (iDestino+iOrigem)/2;
+				int jFora = jOrigem;
+				tabuleiro[iFora][jFora] = null;
+			}
+		}
+		
 	}
 }

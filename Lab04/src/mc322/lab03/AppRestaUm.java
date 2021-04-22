87e65cb6ace9aed56a8728e1517e82050e2f9980
Jéssica Oliveira <jessica.oliveira@conpec.com.br>
2021-04-22 01:18:10 -0300

    pull na dev2

--- Lab04/src/mc322/lab03/AppRestaUm.java
+++ Lab04/src/mc322/lab03/AppRestaUm.java
@@ -4,9 +4,7 @@ public class AppRestaUm {
 	public static void main(String args[]) {
 		Tabuleiro resta1 = new Tabuleiro();
 		resta1.Apresentar();
-		resta1.Mover(1, 3, 4, 3);
-		resta1.Apresentar();
-		resta1.Mover(1, 3, 3, 3);
+		resta1.Mover(6, 2, 6, 4);
 		resta1.Apresentar();
 	}
 }
